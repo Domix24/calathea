78f68a22637ce5183cc18bb65e92b94778a093eb
domix24 <domix24@users.noreply.github.com>
2024-06-08 17:18:26 -0400

    regex

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/:name?', (c) => {
+app.get('/:name{.*}?', (c) => {
   return c.text('Hello ' + (c.req.param('name') || 'Hono')  + '!')
 })
 
